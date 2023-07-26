e7052870ee2be4b34b5eb51ac440d95cd2797f38
Eve <eevis10@hotmail.com>
2023-07-26 19:27:01 +0300

    Clicking on a button in the ScrollView menu auto scrolls said button to center of view

--- Assets/Scripts/GameSelectionController.cs
+++ Assets/Scripts/GameSelectionController.cs
@@ -1,5 +1,7 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -12,16 +14,35 @@ public class GameSelectionController : MonoBehaviour
     private string _gameButtonText;
     private ListView _gameSelectionList;
 
+    private ScrollView _scrollView;
+    private VisualElement _scrollViewElement;
+    private float _snappingDuration = 0.4f;
+
     private void Start()
     {
         InitializeGameList();
+
+        VisualElement[] buttonArray = _scrollView.Children().ToArray();
+        foreach (VisualElement button in buttonArray)
+        {
+            (button as Button).clicked += () => ListButtonClicked(button);
+        }
     }
+
+    //****
+    //
+    //  I haven't yet decided if I should go with a ScrollView or a LisView so I
+    //  have implementations for both for now.
+    //
+    //****
 
     private void InitializeGameList()
     {
+        _rootUI = GetComponent<UIDocument>().rootVisualElement;
+
+        //************ Listview*************
         List<string> GameList = new List<string> { "AA", "BB", "CC", "DDD", "E", "FF", "G", "HHH", "II" };
 
-        _rootUI = GetComponent<UIDocument>().rootVisualElement;
         _gameSelectionList = _rootUI.Q<ListView>("GameList");
 
         _gameSelectionList.makeItem = () =>
@@ -37,5 +58,31 @@ public class GameSelectionController : MonoBehaviour
         };
 
         _gameSelectionList.itemsSource = GameList;
+
+        //****************** SCROLLVIEW ******************
+        _scrollView = _rootUI.Q<ScrollView>("GamesScrollView");
+        _scrollViewElement = _scrollView.contentContainer.hierarchy.parent;
+
+    }
+
+    private void ListButtonClicked(VisualElement button)
+    {
+        ScrollToElement(button);
+    }
+
+    public void ScrollToElement(VisualElement element)
+    {
+        ScrollToTargetValue(GetElementCenterValue(element) - 0.5f * _scrollViewElement.layout.height);
+    }
+
+    float GetElementCenterValue(VisualElement element)
+    {
+        return element.layout.center.y;
+    }
+
+    void ScrollToTargetValue(float target)
+    {
+        Sequence s = DOTween.Sequence();
+        s.Append(DOVirtual.Float(_scrollView.verticalScroller.value, target, _snappingDuration, v => _scrollView.verticalScroller.value = v)).SetEase(Ease.InOutQuad);
     }
 }
