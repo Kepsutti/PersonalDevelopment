6371d3ec095cd14a69038ea597019228dc832d25
Eve <eevis10@hotmail.com>
2023-07-28 17:32:50 +0300

    Added nowScrolling and selectedGameButton for later game selection implementation

--- Assets/Scripts/GameSelectionController.cs
+++ Assets/Scripts/GameSelectionController.cs
@@ -18,9 +18,13 @@ public class GameSelectionController : MonoBehaviour
     private VisualElement _scrollViewElement;
     private float _snappingDuration = 0.4f;
 
+    private bool _nowScrolling;
+    private VisualElement _selectedGameButton;
+
     private void Start()
     {
         InitializeGameList();
+        _nowScrolling = false;
 
         VisualElement[] buttonArray = _scrollView.Children().ToArray();
         foreach (VisualElement button in buttonArray)
@@ -67,22 +71,46 @@ public class GameSelectionController : MonoBehaviour
 
     private void ListButtonClicked(VisualElement button)
     {
-        ScrollToElement(button);
+        if (_nowScrolling)
+            return;
+
+        if (_selectedGameButton != button)
+        {
+            SetSelectedGameButton(button);
+            ScrollToElement(button);
+        }
+        else
+        {
+            Debug.Log("SELECTED");
+        }
     }
 
-    public void ScrollToElement(VisualElement element)
+    private void SetSelectedGameButton(VisualElement button)
+    {
+        _selectedGameButton = button;
+    }
+
+    private void ScrollToElement(VisualElement element)
     {
         ScrollToTargetValue(GetElementCenterValue(element) - 0.5f * _scrollViewElement.layout.height);
     }
 
-    float GetElementCenterValue(VisualElement element)
+    private float GetElementCenterValue(VisualElement element)
     {
         return element.layout.center.y;
     }
 
-    void ScrollToTargetValue(float target)
+    private void ScrollToTargetValue(float target)
     {
+        SetNowScrolling(true);
         Sequence s = DOTween.Sequence();
-        s.Append(DOVirtual.Float(_scrollView.verticalScroller.value, target, _snappingDuration, v => _scrollView.verticalScroller.value = v)).SetEase(Ease.InOutQuad);
+        s.Append(DOVirtual.Float(_scrollView.verticalScroller.value, target, _snappingDuration, v => _scrollView.verticalScroller.value = v))
+            .SetEase(Ease.InOutQuad)
+            .OnComplete(() => SetNowScrolling(false));
+    }
+
+    private void SetNowScrolling(bool value)
+    {
+        _nowScrolling = value;
     }
 }
